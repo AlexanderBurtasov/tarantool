3cac19c3f5ef6351a5a3a259598d26f033adb272
AlexanderBurtasov <pvt.pyle1983@gmail.com>
2020-06-29 03:02:16 +0300

    50 requests per second

--- httpserver/defaults.py
+++ httpserver/defaults.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Defaults(Enum):
   server_url = '0.0.0.0',
   server_port = 5502,
-  request_per_second = 4,
+  request_per_second = 50,
   tarantool_url = '0.0.0.0',
   tarantool_port = 3313,
   storage_name = 'mystorage'
